e4b94195fcc9c4ff52dbb5fc11d11a9bae1f8bad
Ayush-Gupta16 <72401588+Ayush-Gupta16@users.noreply.github.com>
2020-11-15 23:32:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+paperObject=new Paper(50,600,50);
+paperObject.shapeColor=("pink");
+
+groundObject=new Ground(400,670,30,800);
+groundObject.shapecolor=("red");
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+ paperObject.display();
+ groundObject.display();
+ 
+  drawSprites();
+ 
+}
+
+
+
